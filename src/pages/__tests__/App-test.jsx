c1aa2cc511dfe192a5ccb8cf393e44b131e343ee
Dinh Quang Trung <trungdq88@gmail.com>
2015-11-11 00:01:55 +0700

    Add a meaningful test case.

--- src/pages/__tests__/App-test.jsx
+++ src/pages/__tests__/App-test.jsx
@@ -1,17 +1,18 @@
 /**
  * Created by dinhquangtrung on 11/10/15.
  */
-// import React from 'react';
-// import TestUtils from 'react-addons-test-utils';
-// import App from '../App.jsx';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TestUtils from 'react-addons-test-utils';
+import App from '../App.jsx';
 import { expect } from 'chai';
 
 describe('App', () => {
   it('Should have the bootstrap button', () => {
-    // const app = TestUtils.renderIntoDocument(
-    //   <App />
-    // );
-    // const appElement = React.findDOMNode(app);
-    expect(1).to.equal(1);
+    const app = TestUtils.renderIntoDocument(
+      <App />
+    );
+    const appElement = ReactDOM.findDOMNode(app);
+    expect(appElement.querySelectorAll('#the-bootstrap-btn').length).to.equal(1);
   });
 });
